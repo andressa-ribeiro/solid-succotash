bb5e39402f84c665810a67682e77cdb1b1f830c9
andressa-ribeiro <andressajaskulski123@gmail.com>
2025-09-21 11:32:32 -0300

    test: App e 5 testes unitários (loginToolkit)

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,26 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+// src/App.test.js
+import { render, screen, fireEvent } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("mostra título e campos", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(screen.getByText("Login")).toBeInTheDocument();
+  expect(screen.getByPlaceholderText("e-mail")).toBeInTheDocument();
+  expect(screen.getByPlaceholderText("senha")).toBeInTheDocument();
+});
+
+test("login com sucesso", () => {
+  render(<App />);
+  fireEvent.change(screen.getByPlaceholderText("e-mail"), { target: { value: "maria@pucpr.br" } });
+  fireEvent.change(screen.getByPlaceholderText("senha"), { target: { value: "123456" } });
+  fireEvent.click(screen.getByRole("button", { name: /acessar/i }));
+  expect(screen.getByText("Acessado com sucesso!")).toBeInTheDocument();
+});
+
+test("login com erro", () => {
+  render(<App />);
+  fireEvent.change(screen.getByPlaceholderText("e-mail"), { target: { value: "errado@teste.com" } });
+  fireEvent.change(screen.getByPlaceholderText("senha"), { target: { value: "errada" } });
+  fireEvent.click(screen.getByRole("button", { name: /acessar/i }));
+  expect(screen.getByText("Usuário ou senha incorretos!")).toBeInTheDocument();
 });
